fbe3f2ec160c39012097fc98f331e8949f56adff
DanieleAntonaci <danieleanto96@gmail.com>
2023-02-08 16:33:08 +0100

    Aggente funzioni al controller

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
-    //
+    public function index(){
+        return view('pages.index');
+    }
+
+    public function dashboard(){
+        return view('pages.dashboard');
+    }
 }
