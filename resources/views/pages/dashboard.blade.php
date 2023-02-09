6a94fbe1f0cda511143776358270ceca423762ff
DanieleAntonaci <danieleanto96@gmail.com>
2023-02-09 23:15:28 +0100

    Miglioramento stile

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -1,10 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-<a href="{{route('project.create')}}">Add new project</a>
+<div class="btn-add">
+   <a href="{{route('project.create')}}" >Add new project</a>
+</div>
 <ul class="container-list">
    @foreach ($projects as $project)
        <li class="element-list">
+
            <div>
             <a href="{{route('project.show', $project)}}">
                <img src="{{$project -> main_image}}" alt="img">
@@ -13,10 +16,12 @@
                </h2>
             </a>
            </div>
+
            <div>
-              <a href="{{route('project.destroy', $project)}}">X</a>
-               <a href="{{route('project.edit', $project)}}">Edit</a>
+              <a href="{{route('project.destroy', $project)}}" class="delete">DELETE</a>
+               <a href="{{route('project.edit', $project)}}" class="edit">EDIT</a>
            </div>
+           
        </li>
    @endforeach
 </ul>
--- resources/views/pages/riservedPage.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layouts.main-layout')
-
-@section('list-controller')
-    <h1>ciao</h1>
-@endsection
